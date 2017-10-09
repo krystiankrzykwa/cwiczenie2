53547c6b97690f9afa78dcb911055e92dc0909ce
krystiankrzykwa <32266638+krystiankrzykwa@users.noreply.github.com>
2017-10-09 15:27:43 +0200

    cwiczenie2. cz1

---
+++ drzewko/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+   int liczba;
+   cout<<"Podaj liczbe wierszy; "<<endl;
+   cin>>liczba;
+
+   for (int i = 1; i <= liczba; i++)
+   {
+     for (int j=1; j<=(liczba-i)+20; j++)
+     {
+       cout <<" ";
+     }
+
+     for (int k=1; k<=(2*i)-1; k++)
+     {
+       cout<<"*";
+     }
+     cout<<" "<<endl;
+   }
+    return 0;
+}
